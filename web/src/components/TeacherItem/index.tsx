17caf2eaa3bcd2eee8cef443aa75fe5eb515b3fb
Alanna Vanzella <alanna92@gmail.com>
2020-08-06 22:24:56 -0300

    Day 3

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -3,31 +3,50 @@ import React from 'react';
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css';
+import api from '../../services/api';
 
-function TeacherItem() {
+export interface Teacher {
+    avatar: string;
+    bio: string;
+    cost: number;
+    id: number;
+    name: string;
+    subject: string;
+    whatsapp: string;
+}
+
+interface TeacherItemProps {
+    teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({teacher}) => {
+    function createNewConnection() {
+        api.post('connections', { user_id: teacher.id });
+    }
+
     return (
         <article className="teacher-item">
             <header>
-                <img src="https://avatars0.githubusercontent.com/u/1903990?s=460&u=6b2cb281534fb5c81660151be7ab4255a41365c2&v=4" alt="Alanna Vanzella"/>
+                <img src={teacher.avatar} alt={teacher.name}/>
                 <div>
-                    <strong>Alanna Vanzella</strong>
-                    <span>Química</span>
+                    <strong>{teacher.name}</strong>
+                    <span>{teacher.subject}</span>
                 </div>
             </header>
             <p>
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry
-                <br /><br />
-                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                {teacher.bio}
             </p>
             <footer>
                 <p>
                     Preço/hora
-                    <strong>R$ 30,00</strong>
+                    <strong>R$ {teacher.cost}</strong>
                 </p>
-                <button type="button">
+                <a onClick={createNewConnection}
+                    target="blank"
+                    href={`https://wa.me/${teacher.whatsapp}`}>
                     <img src={whatsappIcon} alt="whatsapp"/>
                     Entrar em contato
-                </button>
+                </a>
             </footer>
         </article>
     )
--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,36 +1,82 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
 
 import PageHeader from '../../components/PageHeader';
 
 import './styles.css';
-import TeacherItem from '../../components/TeacherItem';
+import TeacherItem, { Teacher } from '../../components/TeacherItem';
+import Input from '../../components/Input';
+import Select from '../../components/Select';
+import api from '../../services/api';
 
 function TeacherList() {
+    const [subject, setSubject] = useState('');
+    const [weekDay, setWeekDay] = useState('');
+    const [time, setTime] = useState('');
+    const [teachers, setTeachers] = useState([]);
+
+    async function handleSearch(e: FormEvent) {
+        e.preventDefault();
+
+        const response = await api.get('classes', {
+            params: {
+                subject,
+                week_day: weekDay,
+                time,
+            }
+        });
+
+        setTeachers(response.data);
+    }
+
     return (
         <div id="page-teacher-list" className="container">
             <PageHeader title="Esses são os proffys disponíveis">
-                <form id="search-teachers">
-                    <div className="input-block">
-                        <label htmlFor="subject">Matéria</label>
-                        <input type="text" name="subject" id="sucject"/>
-                    </div>
-                    <div className="input-block">
-                        <label htmlFor="week_day">Dia da Semana</label>
-                        <input type="text" name="week_day" id="week_day"/>
-                    </div>
-                    <div className="input-block">
-                        <label htmlFor="hour">Horário</label>
-                        <input type="text" name="hour" id="hour"/>
-                    </div>
+                <form id="search-teachers" onSubmit={handleSearch}>
+                    <Select
+                        label="Matéria" 
+                        name="subject"
+                        value={subject}
+                        onChange={e => setSubject(e.target.value)}
+                        options={[
+                            { value: "Artes", label: "Artes"},
+                            { value: "Biologia", label: "Biologia"},
+                            { value: "Matematica", label: "Matematica"},
+                            { value: "Fisica", label: "Fisica"},
+                            { value: "Portugues", label: "Portugues"},
+                            { value: "Ingles", label: "Ingles"},
+                        ]}
+                    />
+                    <Select
+                        label="Dia da semana" 
+                        name="week_day"
+                        value={weekDay}
+                        onChange={e => setWeekDay(e.target.value)}
+                        options={[
+                            { value: "0", label: "Domingo"},
+                            { value: "1", label: "Segunda"},
+                            { value: "2", label: "Terça"},
+                            { value: "3", label: "Quarta"},
+                            { value: "4", label: "Quinta"},
+                            { value: "5", label: "Sexta"},
+                            { value: "6", label: "Sábado"},
+                        ]}
+                    />
+                    <Input 
+                        type="time" 
+                        label="Hora" 
+                        name="time" 
+                        value={time}
+                        onChange={e => setTime(e.target.value)}
+                    />
+
+                    <button type="submit">Buscar</button>
                 </form>
             </PageHeader>
 
             <main>
-               <TeacherItem />
-               <TeacherItem />
-               <TeacherItem />
-               <TeacherItem />
-               <TeacherItem />
+                {teachers.map((teacher: Teacher) => {
+                    return <TeacherItem key={teacher.id} teacher={teacher} />
+                })}
             </main>
         </div>
     );
